e6dc4559e29c17a55fdfff1324f50eb95a1dc5dc
Shahwaiz512 <159876458+Shahwaiz512@users.noreply.github.com>
2024-02-28 10:22:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// const inputBox = document.querySelector('#to-do');
+// const listContainer = document.querySelector('.list-container');
+// function addTask(){
+//     if(inputBox.value == ''){
+//         alert('You Must write something---')
+//     }else{
+//     let li = document.createElement('li');
+//     li.innerHTML = inputBox.value;
+//     listContainer.appendChild(li);
+//     }
+//     inputBox.value= ''
+// }
